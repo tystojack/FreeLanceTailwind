40264843203b7287782faa7cb9df17518364a161
tystojack <tystojack@gmail.com>
2023-09-11 18:09:09 -0500

    improvement

--- app/components/MainHome.tsx
+++ app/components/MainHome.tsx
@@ -2,15 +2,42 @@ import React from 'react'
 
 function MainHome() {
   return (
-    <div className='w-screen flex justify-center items-center '>
+    <div className='w-screen flex justify-center items-center flex-col  '>
 
-    <div className=' bg-stone-900 flex items-center justify-between w-screen max-w-xl border-4 border-purple-900  '>
-    <div className='w-full h- flex items-center   justify-center'>
+    <div className=' bg-black flex items-center justify-between w-screen max-w-xl border-2 border-purple-600 rounded-lg mt-2'>
+    <div className='w-full  flex items-center   justify-center'>
         <h1 className='text-2xl font-bold text-white'>Hello</h1>
         </div>
-        <div className='w-2/4'>
+        <div className='w-1/4 mr-8'>
 
-        <img src="/computer.png" alt="computer" className='object-contain'/>
+        <img src="/computer.png" alt="computer" className='object-contain '/>
+        </div>
+    </div>
+    <div className=' bg-black flex items-center justify-between w-screen max-w-xl border-2 border-purple-600  rounded-lg mt-2'>
+    <div className='w-full  flex items-center   justify-center'>
+        <h1 className='text-2xl font-bold text-white'>Hello</h1>
+        </div>
+        <div className='w-1/4 mr-8'>
+
+        <img src="/computer.png" alt="computer" className='object-contain '/>
+        </div>
+    </div>
+    <div className=' bg-black flex items-center justify-between w-screen max-w-xl border-2 border-purple-600  rounded-lg mt-2'>
+    <div className='w-full  flex items-center   justify-center'>
+        <h1 className='text-2xl font-bold text-white'>Hello</h1>
+        </div>
+        <div className='w-1/4 mr-8'>
+
+        <img src="/computer.png" alt="computer" className='object-contain '/>
+        </div>
+    </div>
+    <div className=' bg-black flex items-center justify-between w-screen max-w-xl border-2 border-purple-600  rounded-lg mt-2'>
+    <div className='w-full  flex items-center   justify-center'>
+        <h1 className='text-2xl font-bold text-white'>Hello</h1>
+        </div>
+        <div className='w-1/4 mr-8'>
+
+        <img src="/computer.png" alt="computer" className='object-contain '/>
         </div>
     </div>
     </div>
