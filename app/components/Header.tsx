2cd15238e234949a252787cc6c389055c195105a
Tyler <tystojack@gmail.com>
2023-09-14 08:48:53 -0500

    start of the burger

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -5,14 +5,23 @@ import React from 'react'
 function Header() {
   return (
 <nav className='bg-purple-900 h-16 fixed top-0 w-screen  flex justify-between items-center z-50 '>
-<div>
-  <h3 className='text-neutral-50 '>
+<div className='h-3/4 w-48 '>
+  {/* <h3 className='text-neutral-50 '>
 
-  about
+   Code Clap
  
-    </h3>
+    </h3> */}
+    <img className='object-contain max-h-full ml-4' src="C.png"/>
+    
+
 </div>
-<div className=" invisible lg:visible lg:flex lg:mr-8 lg:space-x-4 lg:font-semibold lg:text-xl ">
+<div >
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className='fill-current text-stone-50 h-10 w-10 md:hidden'>
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+</svg>
+
+</div>
+<div className=" hidden md:visible md:block  invisible lg:visible lg:flex lg:mr-8 lg:space-x-4 lg:font-semibold lg:text-xl ">
   <h3 className='text-neutral-50'>
    <Link href="/About">
    about
